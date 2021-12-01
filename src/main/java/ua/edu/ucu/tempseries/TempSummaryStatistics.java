4ae3a80a6cb53fdbf54c8c67525b3674f3ab36eb
cosssec <daryna.kosyk@ucu.edu.ua>
2021-12-01 22:32:22 +0200

    Update TempSummaryStatistics.java

--- src/main/java/ua/edu/ucu/tempseries/TempSummaryStatistics.java
+++ src/main/java/ua/edu/ucu/tempseries/TempSummaryStatistics.java
@@ -6,7 +6,11 @@ public class TempSummaryStatistics {
     private final double minTemp;
     private final double maxTemp;
 
-    public TempSummaryStatistics(double avgTemp, double devTemp, double minTemp, double maxTemp) {
+    public TempSummaryStatistics
+            (double avgTemp,
+             double devTemp,
+             double minTemp,
+             double maxTemp) {
         this.avgTemp = avgTemp;
         this.devTemp = devTemp;
         this.minTemp = minTemp;
@@ -27,8 +31,4 @@ public class TempSummaryStatistics {
     public double getMaxTemp() {
         return maxTemp;
     }
-
-
-
-
 }
